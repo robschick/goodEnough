af144a4fd7865d6c8be5bd688e7d6ab281da8159
Rob Schick <robschick@gmail.com>
2017-08-03 16:10:50 -0400

    Fix Incorrect Saving of CSV File

--- src/summarizeData.R
+++ src/summarizeData.R
@@ -3,4 +3,4 @@
 # and then saves them to the results file
 species <- read.csv(file = '../data/MOCK_DATA.csv')
 speciesSummary <- species[species$randomData > 2, ]
-save(speciesSummary, file = '../results/dataSummary.csv')
+write.csv(speciesSummary, file = '../results/dataSummary.csv')
